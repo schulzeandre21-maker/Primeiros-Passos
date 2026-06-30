9d7350a7a1b9f1da5b1bff072b4dcfd7cc209f58
André S <schulzeandre21@gmail.com>
2026-06-29 21:18:36 -0300

    tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,21 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  //  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  // if (html.classList.contains("light")) {
+  //se tiver em light mode, adcicionar a imagem ligth
+  //   img.setAttribute("src",)
+  //  } else {
+  //se tiver sem light mode, manter a imagem normal
+  //  img.setAttribute("src",)
+  // }
+
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+}
